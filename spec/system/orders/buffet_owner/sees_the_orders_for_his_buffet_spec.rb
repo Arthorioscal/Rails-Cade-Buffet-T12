881fdbc44d47052de35f4da2512753335a2c3d6c
ArthurScortegagna <arthurscortegagna@gmail.com>
2024-05-01 10:10:57 -0300

    Mais testes de visualização dos pedidos por parte do dono do buffet concluídos

--- spec/system/orders/buffet_owner/sees_the_orders_for_his_buffet_spec.rb
+++ spec/system/orders/buffet_owner/sees_the_orders_for_his_buffet_spec.rb
@@ -49,4 +49,77 @@ describe 'Buffet owner sees the orders made for his buffet' do
         expect(page).to have_content('Rua do Evento, 123')
         expect(page).to have_content('bigboss@gmail.com')
     end
+
+    it 'and is sorted by status' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        client = User.create!(email: 'bigboss@gmail.com', password: 'bigboss', name: 'Big Boss', cpf: '11543210023', role: :client)
+        order = Order.create!(event_date: '10/10/2099', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client)
+        client2 = User.create!(email: 'TheBoss@mail.com', password: 'theboss', name: 'The Boss', cpf: '99692188086', role: :client)
+        order2 = Order.create!(event_date: '10/10/2098', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client2, status: :confirmed)
+        
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Meu Buffet'
+        click_on 'Pedidos'
+
+        expect(page.body.index("Status do Pedido: Aguardando Avaliação")).to be < page.body.index("Status do Pedido: Confirmado")
+        expect(page).to have_content("Pedido: #{order.order_code}")
+        expect(page).to have_content("Status do Pedido: Aguardando Avaliação")
+        expect(page).to have_content("Pedido: #{order2.order_code}")
+        expect(page).to have_content("Status do Pedido: Confirmado")
+    end
+
+    it 'and has two orders for the same day' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        client = User.create!(email: 'bigboss@gmail.com', password: 'bigboss', name: 'Big Boss', cpf: '11543210023', role: :client)
+        order = Order.create!(event_date: '10/10/2099', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client)
+        client2 = User.create!(email: 'TheBoss@mail.com', password: 'theboss', name: 'The Boss', cpf: '99692188086', role: :client)
+        order2 = Order.create!(event_date: '10/10/2099', estimated_guests: 60, details: 'Festa de aniversário de 60 anos', event_address: 'Rua dos Eventos, 133', buffet: buffet, event: event, user: client2, status: :confirmed)
+
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Meu Buffet'
+        click_on 'Pedidos'
+        click_on "Pedido: #{order.order_code}"
+
+        expect(page).to have_content('Tem um outro pedido para essa mesma data.')
+        expect(page).to have_content('Festa de Aniversário')
+        expect(page).to have_content('10/10/2099')
+        expect(page).to have_content('50')
+    end
+
+    it 'and has no orders' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Meu Buffet'
+        click_on 'Pedidos'
+
+        expect(page).to have_content('Você ainda não tem nenhum pedido registrado')
+    end
 end
