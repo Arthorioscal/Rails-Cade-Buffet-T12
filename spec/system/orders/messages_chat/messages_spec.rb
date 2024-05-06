df6497376c38a6f84be8d9f9f29ab3b03237003f
ArthurScortegagna <arthurscortegagna@gmail.com>
2024-05-06 14:07:19 -0300

    Testes de chat feitos

--- spec/system/orders/messages_chat/messages_spec.rb
+++ spec/system/orders/messages_chat/messages_spec.rb
@@ -25,4 +25,89 @@ describe 'Messages tests' do
         expect(page).to have_content('Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.')
         expect(page).to have_content('Big Boss')
     end
+
+    it 'buffet owner sees the message' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        client = User.create!(email: 'bigboss@gmail.com', password: 'bigboss', name: 'Big Boss', cpf: '11543210023', role: :client)
+        order = Order.create!(event_date: '10/10/2099', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client, 
+                            status: 1, valid_until: 7.days.from_now, extra_fee: 100, discount: 0, description: 'Festa de aniversário de 50 anos', order_payment_method: 'Cartão de Crédito', final_price: 2000)
+        message = Message.create!(content: 'Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.', order: order, user: client)
+
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Meu Buffet'
+        click_on 'Pedidos'
+        click_on "Pedido: #{order.order_code}"
+
+        expect(page).to have_content('Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.')
+        expect(page).to have_content('Big Boss')
+    end
+
+    it 'buffet owner replies to the message' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        client = User.create!(email: 'bigboss@gmail.com', password: 'bigboss', name: 'Big Boss', cpf: '11543210023', role: :client)
+        order = Order.create!(event_date: '10/10/2099', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client, 
+                            status: 1, valid_until: 7.days.from_now, extra_fee: 100, discount: 0, description: 'Festa de aniversário de 50 anos', order_payment_method: 'Cartão de Crédito', final_price: 2000)
+        message = Message.create!(content: 'Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.', order: order, user: client)
+
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Meu Buffet'
+        click_on 'Pedidos'
+        click_on "Pedido: #{order.order_code}"
+        fill_in 'message_content', with: 'Olá, tenho sim disponibildade para esta data.'
+        click_on 'Enviar mensagem'
+    
+        expect(page).to have_content('Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.')
+        expect(page).to have_content('Big Boss')
+        expect(page).to have_content('Olá, tenho sim disponibildade para esta data.')
+        expect(page).to have_content('Buffet do Snake')
+    end
+
+    it 'client sees the reply' do
+        user = User.create!(email: 'raiden@mgs.com', password: 'solidsnakefan', role: :buffet_owner)
+        buffet = Buffet.create!(brand_name: 'Buffet do Snake', corporate_name: 'Buffet do Snake Ltda', cnpj: '45195101000101',
+        phone: '11999999999', email: 'solidsnakefans@mail.com', address: 'Rua do Buffet, 123', neighborhood: 'Bairro do Buffet',
+        state: 'São Paulo', city: 'São Paulo', zip_code: '12345678', description: 'Buffet especializado em festas de aniversário',
+        payment_methods: 'Dinheiro, cartão de crédito e débito', user: user)
+        event = Event.create!(name: 'Festa de Aniversário', description: 'Festa de aniversário com bolo, doces e salgados', min_people: 10,
+        max_people: 100, duration: 180, menu: 'Bolo, doces, salgados, refrigerante, vinho', alcohol: true, decoration: true, parking_service: true,
+        at_buffet_location: true, buffet: buffet)
+        event_price = EventPrice.create!(wd_price: 2000, wd_add_person_price: 70, wd_extra_hour_price: 100, we_price: 2500, we_add_person_price: 80, we_extra_hour_price: 150, event: event)
+        client = User.create!(email: 'bigboss@gmail.com', password: 'bigboss', name: 'Big Boss', cpf: '11543210023', role: :client)
+        order = Order.create!(event_date: '10/10/2099', estimated_guests: 50, details: 'Festa de aniversário de 50 anos', event_address: 'Rua do Evento, 123', buffet: buffet, event: event, user: client, 
+                            status: 1, valid_until: 7.days.from_now, extra_fee: 100, discount: 0, description: 'Festa de aniversário de 50 anos', order_payment_method: 'Cartão de Crédito', final_price: 2000)
+        message = Message.create!(content: 'Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.', order: order, user: client)
+        reply = Message.create!(content: 'Olá, tenho sim disponibildade para esta data.', order: order, user: user)
+
+        login_as client, scope: :user
+        visit root_path
+        click_on 'Meus Pedidos'
+        click_on "Pedido: #{order.order_code}"
+        fill_in 'message_content', with: 'Ótimo, muito obrigado!'
+        click_on 'Enviar mensagem'
+        
+        expect(page).to have_content('Olá, gostaria de saber se vocês têm disponibilidade para a data do evento.')
+        expect(page).to have_content('Big Boss')
+        expect(page).to have_content('Olá, tenho sim disponibildade para esta data.')
+        expect(page).to have_content('Buffet do Snake')
+        expect(page).to have_content('Ótimo, muito obrigado!')
+        expect(page).to have_content('Big Boss')
+    end
 end
